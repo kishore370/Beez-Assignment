13d1222e598e2ab047e52985f712c8b9c9377166
kishore370 <109137089+kishore370@users.noreply.github.com>
2022-07-13 11:54:23 +0530

    Add files via upload

---
+++ grade.cs
@@ -0,0 +1,35 @@
+﻿string input;
+Console.WriteLine("These are the grades ");
+Console.WriteLine("Excellent");
+Console.WriteLine("VeryGood");
+Console.WriteLine("Good");
+Console.WriteLine("Average");
+Console.WriteLine("Fail");
+Console.WriteLine("INPUT THE FIRSTLETTER OF THE GRADE=");
+
+input = Console.ReadLine();
+if (input == "E" || input == "e")
+{
+    Console.WriteLine("Excellent");
+}
+else if (input == "V" || input == "v")
+{
+    Console.WriteLine("VeryGood");
+}
+else if (input == "G" || input == "g")
+{
+    Console.WriteLine("Good");
+}
+else if (input == "A" || input == "a")
+{
+    Console.WriteLine("Average");
+}
+else if (input == "F" || input == "f")
+{
+    Console.WriteLine("Fail");
+}
+else
+{
+    Console.WriteLine("invalid input");
+}
+Console.ReadLine();
